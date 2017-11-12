51ba1ec9f503f8d5a62eb25a9571bcb481284ed1
domino <lolosiowo2>
2017-11-12 18:28:21 +0100

    Show action useful method

--- src/main/java/xyz/yeahbunny/adventurer/utils/action/ShowAction.java
+++ src/main/java/xyz/yeahbunny/adventurer/utils/action/ShowAction.java
@@ -1,5 +1,7 @@
 package xyz.yeahbunny.adventurer.utils.action;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Set;
 
 public class ShowAction<DATA> extends Action<DATA> {
@@ -12,5 +14,9 @@ public class ShowAction<DATA> extends Action<DATA> {
 
     public void setUsersToNotify(Set<String> usersToNotify) {
         this.usersToNotify = usersToNotify;
+    }
+
+    public void setUsersToNotify(String ...usersToNotify) {
+        this.usersToNotify = new HashSet<>(Arrays.asList(usersToNotify));
     }
 }
