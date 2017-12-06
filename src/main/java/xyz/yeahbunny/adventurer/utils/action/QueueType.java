c09e7398fd0faa4d0a5f07ab9dd4b9905657709b
domino <lolosiowo2>
2017-12-06 20:07:33 +0100

    queue type enum

---
+++ src/main/java/xyz/yeahbunny/adventurer/utils/action/QueueType.java
@@ -0,0 +1,32 @@
+package xyz.yeahbunny.adventurer.utils.action;
+
+import com.fasterxml.jackson.annotation.JsonValue;
+
+public enum QueueType {
+    INVALID("invalid"),
+    API("apiQueue"),
+    CHARACTER_QUEUE("characterQueue"),
+    POSITION_QUEUE("positionQueue");
+
+    private final String name;
+
+    QueueType(String s) {
+        name = s;
+    }
+
+    public static QueueType byName(String name) {
+        for (QueueType actionType : values()) {
+            if (actionType.name.equals(name)) {
+                return actionType;
+            }
+        }
+        return INVALID;
+    }
+
+    @JsonValue
+    @Override
+    public String toString() {
+        return this.name;
+    }
+
+}
